b410f2e87c69b5d892dedf1970f4cbf803936b4f
ville <ville.prami@aalto.fi>
2023-08-01 23:19:31 +0300

    main functionality and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,65 @@
+import { useState } from "react"
+import axios from "axios"
+
 import Footer from "./components/Footer"
 
+import iconDice from "../images/icon-dice.svg"
+import patternDividerDesktop from "../images/pattern-divider-desktop.svg"
+
+interface Advice {
+  id: number
+  advice: string
+}
+
+const defaultAdvice: Advice = {
+  id: 0,
+  advice:
+    "It is easy to sit up and take notice, what's difficult is getting up and taking action.",
+}
+
+const errorAdvice: Advice = {
+  id: -1,
+  advice: "Failed to fetch new advice. Try again later.",
+}
+
+//figure out error and default states
+//loading state (spinning dice?)
+
 const App = () => {
+  const [adviceSlip, setAdviceSlip] = useState<Advice>(defaultAdvice)
+
+  const updateAdvice = async () => {
+    try {
+      const { data } = await axios.get("https://api.adviceslip.com/advice")
+      setAdviceSlip(data.slip)
+      console.log(data.slip)
+    } catch (error) {
+      console.log(error)
+      setAdviceSlip(errorAdvice)
+    }
+  }
+
   return (
-    <div className="h-screen">
+    <>
+      <main className="h-screen flex items-center justify-center font-manrope bg-darkBlue">
+        <div className="bg-darkGrayishBlue inline-flex flex-col items-center rounded-2xl relative p-8 m-4 max-w-[540px]">
+          <h1 className="mt-4 uppercase text-neonGreen tracking-[0.2em] text-xs">
+            Advice #{adviceSlip.id}
+          </h1>
+          <p className="mt-6 text-2xl sm:text-[28px]/[42px] text-lightCyan text-center">
+            "{adviceSlip.advice}"
+          </p>
+          <img className="mt-6 sm:mt-8 mb-8" src={patternDividerDesktop} />
+          <button
+            className="absolute bottom-0 right-1/2 translate-x-1/2 translate-y-1/2 bg-neonGreen rounded-full p-5 hover:shadow-[0px_0px_30px_0px] hover:shadow-neonGreen"
+            onClick={updateAdvice}
+          >
+            <img src={iconDice} />
+          </button>
+        </div>
+      </main>
       <Footer />
-    </div>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightCyan: "hsl(193, 38%, 86%)",
+        neonGreen: "hsl(150, 100%, 66%)",
+        grayishBlue: "hsl(217, 19%, 38%)",
+        darkGrayishBlue: "hsl(217, 19%, 24%)",
+        darkBlue: "hsl(218, 23%, 16%)",
+      },
+      fontFamily: {
+        manrope: ['"Manrope"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
